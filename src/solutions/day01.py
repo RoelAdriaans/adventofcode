ab6d62faa028d8709e32ff1654d8506f2ec6536b
Roel Adriaans <roel@road-support.nl>
2019-06-30 17:24:37 +0200

    Add code for day01, part 01

--- src/solutions/day01.py
+++ src/solutions/day01.py
@@ -1,3 +1,10 @@
 class Day1:
-    def solve_01(self, input):
-        return None
+    def solve_01(self, input_data: str) -> int:
+        parts = input_data.split()
+        total = 0
+        for part in parts:
+            if part[0] == "+":
+                total += int(part[1:])
+            if part[0] == "-":
+                total -= int(part[1:])
+        return total
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -6,7 +6,12 @@ from solutions.day01 import Day1
 class Test_Day01:
     @pytest.mark.parametrize(
         "input_data,expected_result",
-        [("+1, +1, +1", 3), ("+1, +1, -2", 0), ("-1, -2, -3", -6)],
+        [
+            ("+1 -2 +3 +1", 3),
+            ("+1 +1 +1", 3),
+            ("+1 +1 -2", 0),
+            ("-1 -2 -3", -6),
+        ],
     )
     def test_day01(self, input_data, expected_result):
         d1 = Day1()
