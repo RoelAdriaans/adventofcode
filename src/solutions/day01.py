86119dd8f5aa3d35a0c0e54603543bc318173ab4
Roel Adriaans <roel@road-support.nl>
2019-06-30 17:12:53 +0200

    Add black to tox, clenaup code and test

--- src/solutions/day01.py
+++ src/solutions/day01.py
@@ -1,4 +1,3 @@
 class Day1:
     def solve_01(self, input):
-        print(f"Solving day 1 with input {input}")
-        return 3
+        return None
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -2,19 +2,13 @@ import pytest
 
 from solutions.day01 import Day1
 
-# [("+1, +1, +1", 3), ("+1, +1, -2", 0), ("-1, -2, -3", -6)],
-
 
 class Test_Day01:
     @pytest.mark.parametrize(
-        "input_data,expected_result", [("+1, +1, +1", 3)]
+        "input_data,expected_result",
+        [("+1, +1, +1", 3), ("+1, +1, -2", 0), ("-1, -2, -3", -6)],
     )
     def test_day01(self, input_data, expected_result):
         d1 = Day1()
         res = d1.solve_01(input_data)
         assert expected_result == res
-
-    def test_day01_will_fail(self):
-        d1 = Day1()
-        res = d1.solve_01("2=3=34=45")
-        assert 4 == res
