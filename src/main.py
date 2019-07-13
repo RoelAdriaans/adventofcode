acff38c95a5bc410aafd56bf17d3beb5d2698850
Roel Adriaans <roel@road-support.nl>
2019-07-13 17:41:13 +0200

    Remove name from modules tuple, compute from Class

--- src/main.py
+++ src/main.py
@@ -2,30 +2,32 @@ from solutions import day01
 
 import click
 
-modules = {
-    (day01.Day1PartA, "day01_a", ("day01/day_01_part01.txt", "day01/day_01_test.txt")),
-    (day01.Day1PartB, "day01_b", ("day01/day_01_part01.txt",)),
-}
+modules = [
+    (day01.Day1PartA, ("day01/day_01_part01.txt", "day01/day_01_test.txt")),
+    (day01.Day1PartB, ("day01/day_01_part01.txt",)),
+]
 
 
 @click.command()
-@click.option("--module", type=click.Choice([i[1] for i in modules]), required=True)
+@click.option(
+    "--module", type=click.Choice([i[0].__name__ for i in modules]), required=True
+)
 def main(module):
     """
     Simple program that runs a module from the advent of code
     """
-    item = [item for item in modules if item[1] == module]
+    item = [item for item in modules if item[0].__name__ == module]
     if not item:
         print(f"Module {module} not found")
         return False
     else:
         item = item[0]
 
-    print(f"Running module '{item[1]}'")
+    print(f"Running module '{item[0].__name__}'")
 
     class_to_run = item[0]
     class_instance = class_to_run()
-    for filename in item[2]:
+    for filename in item[1]:
         res = class_instance(filename)
         print(f"Result for {filename} -> {res}")
 
