5f15191280af4880466173f23795652a7b2f2124
Roel Adriaans <roel@road-support.nl>
2019-07-14 12:07:49 +0200

    Add requirements.txt to project, and read from setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="adventofcode2018",
     author="Roel Adriaans",
     author_email="roel@adriaans.org",
     url="https://github.com/roeladriaans",
-    version="0.0.1",
+    version="0.0.2",
     entry_points={"console_scripts": ["adventofcode = main:main"]},
     packages=find_packages(where="src", exclude=["tests.*", "test*"]),
     package_dir={"": "src"},
-    install_requires=["Click"],
+    install_requires=requirements,
     include_package_data=True,
 )
