46621be42f52d80b38a16a43f6ef29b1fe51b0c8
Roel Adriaans <roel@road-support.nl>
2019-08-31 16:31:10 +0200

    Add the solution for day 09 a

---
+++ src/solutions/day09.py
@@ -0,0 +1,168 @@
+from utils.abstract import FileReaderSolution
+from collections import defaultdict, Counter
+
+
+class Day09:
+    players = None
+
+    def __init__(self):
+        self.players = Counter()
+        self.grid = list()
+        self.current_marble = 0
+        self.available_marbles = list()
+
+    def _compute_posisition(self) -> int:
+        """
+        Returns the position for the new marble that is going to be places in the grid.
+
+        Now we have to compute the positions.
+        Each Elf takes a turn placing the lowest-numbered remaining marble into
+        the circle between the marbles that are 1 and 2 marbles clockwise of the
+        current marble.
+        (When the circle is large enough, this means that there is one marble between
+        the marble that was just placed and the current marble.) The marble that was
+        just placed then becomes the current marble.
+
+        This is based on the `self.current_marble`. That is the last marble that is
+        played.
+        :return:
+        """
+        grid_size = len(self.grid)
+        if grid_size == 0:
+            return 0
+        if grid_size == 1:
+            return 1
+
+        current_index = self.grid.index(self.current_marble)
+        current_index += 2
+        return self._get_index(current_index)
+
+    def _get_index(self, position: int) -> int:
+        """
+        Return the index in the grid for a position. If the posisition is outside
+        the grid, the index will be reset
+        """
+        if position < 0:
+            # position = len(self.grid) - position
+            # return position
+            raise ValueError("Negative not supported!")
+
+        grid_size = len(self.grid)
+        if position == grid_size:
+            # If we are on the right edge, return this value
+            return position
+
+        current_index = position % grid_size
+        return current_index
+
+    def _remove_marble(self) -> int:
+        """
+        Remove marble 7 positions from the `current_marble` and
+        update `current_marble` with the new position
+
+        :return: The value of the removed marble
+        """
+        self.available_marbles.pop(0)
+        score = self.current_marble
+
+        current_index = self.grid.index(self.current_marble - 1)
+        if current_index < 7:
+            # Eer de rest:
+            left = 7 - current_index
+            search_index = len(self.grid) - left
+        else:
+            search_index = current_index - 7
+        remove_position = self._get_index(search_index)
+
+        # Add the value of that marble to the score
+        score += self.grid[remove_position]
+
+        # Current marble the is next to the one we removed just now
+        self.current_marble = self.grid[remove_position + 1]
+
+        self.grid.pop(remove_position)
+
+        return score
+
+    def _insert_marble(self, position: int) -> int:
+        """
+        Insert the first abailable marble at the position `position`.
+        If a marble is already at this position, it will be shifted to the right.
+
+        Begin state:
+        0 2 1 3
+        Insert marble 4 at position 1:
+        0 4 2 1 3
+
+        :param position: Position to add the marble to
+        :return: The value of the marble that was added
+        """
+        marble = self.available_marbles.pop(0)
+        self.grid.insert(position, marble)
+        return marble
+
+    def play_game(self, players, last_marble):
+        """ Compute the result from the number of `players` and the worth in points
+        of the `last_marble`
+
+        :param players: The number of players
+        :param last_marble: The last marble is worth so many points
+        """
+        self.current_marble = 0
+        self.available_marbles = list(range(0, last_marble + 1))
+        current_player = 0
+
+        # Ad the first marble to the board
+        position = self._compute_posisition()
+        self._insert_marble(position=position)
+
+        while self.available_marbles:
+            if self.current_marble >= last_marble:
+                return
+
+            current_player += 1
+            if current_player > players:
+                current_player = 1
+
+            # We need to figure out where to place the next marble
+            position = self._compute_posisition()
+            self.current_marble += 1
+            if self.current_marble > 1 and (self.current_marble % 23 == 0):
+                # Current marble is a multiple of 23!
+                # The current player gets the current marble value added to their score
+
+                # Remove the marble, add this value to the score
+                self.players[current_player] += self._remove_marble()
+            else:
+                self.current_marble = self._insert_marble(position=position)
+
+
+class Day09PartA(Day09, FileReaderSolution):
+    def compute_result(self, players: int, last_marble: int) -> int:
+        """
+        Compute the result from the number of `players` and the worth in points
+        of the `last_marble`
+
+        :param players: The number of players
+        :param last_marble: The last marble is worth so many points
+        :returns the highest score
+        """
+        self.play_game(players, last_marble)
+        return self.compute_score()
+
+    def compute_score(self) -> int:
+        """ Compute the score for a player"""
+        best_player = self.players.most_common(1)
+        return best_player[0][1]
+
+    def solve(self, input_data: str) -> int:
+        input_data = input_data.split()
+        players = int(input_data[0])
+        marble = int(input_data[6])
+        score = self.compute_result(players=players, last_marble=marble)
+        return score
+
+
+class Day09PartB(Day09, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        raise NotImplementedError
---
+++ tests/solutions/test_day09_a.py
@@ -0,0 +1,65 @@
+import pytest
+
+from solutions.day09 import Day09PartA
+
+
+class TestDay09PartA:
+    @pytest.mark.parametrize(
+        ("grid", "current_marble", "expected_result"),
+        [
+            ([], 0, 0),  # Step 0  (0) - Check
+            ([0], 0, 1),  # Step 1 (0,1) - Check
+            ([0, 1], 1, 1),  # Step 2 (0,2,1) - Check
+            ([0, 2, 1], 2, 3),  # Step 3 (0,2,1,3) - Check
+            ([0, 4, 2, 1, 3], 4, 3),  # Step 5
+            ([0, 8, 4, 2, 5, 1, 6, 3, 7], 8, 3),
+            ([0, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 7], 13, 13),  # check
+            ([0, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7], 14, 15),  # check
+            ([0, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15], 15, 1),  # Check
+            ([0, 16, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15], 16, 3),  # Ck
+        ],
+    )
+    def test_day09a_compute_compute_posisition(
+        self, grid, current_marble, expected_result
+    ):
+        # Compute the next position. In the grid, with current marble at as a number,
+        # we search for the  posistion for the *next* marble.
+        # Check -- Testcase is good
+        solution = Day09PartA()
+        solution.current_marble = current_marble
+        solution.grid = grid
+        result = solution._compute_posisition()
+        assert result == expected_result
+
+    def test_day09a_grid_after_simple_game(self):
+        """ Play the same in the example and assert that the end game of the
+        board is the same."""
+        expected = "0 16 8 17 4 18 19 2 24 20 25 10 21 5 22 11 1 12 6 13 3 14 7 15"
+        solution = Day09PartA()
+        solution.play_game(9, 25)
+
+        grid_txt = " ".join([str(x) for x in solution.grid])
+
+        assert expected == grid_txt
+
+    @pytest.mark.parametrize(
+        ("players", "last_marble", "expected_result"),
+        [
+            (9, 25, 32),
+            (10, 1618, 8317),
+            (13, 7999, 146373),
+            (17, 1104, 2764),
+            (21, 6111, 54718),
+            (30, 5807, 37305),
+        ],
+    )
+    def test_day09a_solve(self, players, last_marble, expected_result):
+        solution = Day09PartA()
+        result = solution.compute_result(players, last_marble)
+        assert result == expected_result
+
+    def test_day09a_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day09PartA()
+        res = solution("day_09/day09.txt")
+        assert res == 424112
