ae45099f5c0e88960636d3af1e82aa611ae8e8d9
Roel Adriaans <roel.adriaans@ordina.nl>
2021-01-02 22:18:34 +0100

    Remove pytest skip, since we're going to implement the code anytime soon

--- template/{{cookiecutter.advendofcode2020}}/tests/solutions/test_{{cookiecutter.file_name}}_a.py
+++ template/{{cookiecutter.advendofcode2020}}/tests/solutions/test_{{cookiecutter.file_name}}_a.py
@@ -1,17 +1,17 @@
 import pytest
 
-from adventofcode2020.solutions.{{cookiecutter.file_name}} import {{cookiecutter.class_name}}PartA
+from adventofcode2020.solutions.{{cookiecutter.file_name}} import (
+    {{cookiecutter.class_name}}PartA,
+)
 
 
 class Test{{cookiecutter.class_name}}PartA:
-    @pytest.mark.skip("This code is not yet implemented.")
     @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
     def test_{{cookiecutter.file_name}}a_solve(self, input_data, expected_result):
         solution = {{cookiecutter.class_name}}PartA()
         result = solution.solve(input_data)
         assert result == expected_result
 
-    @pytest.mark.skip("This code is not yet implemented.")
     def test_{{cookiecutter.file_name}}a_data(self):
         """ Result we got when we did the real solution """
         solution = {{cookiecutter.class_name}}PartA()
--- template/{{cookiecutter.advendofcode2020}}/tests/solutions/test_{{cookiecutter.file_name}}_b.py
+++ template/{{cookiecutter.advendofcode2020}}/tests/solutions/test_{{cookiecutter.file_name}}_b.py
@@ -1,17 +1,17 @@
 import pytest
 
-from adventofcode2020.solutions.{{cookiecutter.file_name}} import {{cookiecutter.class_name}}PartB
+from adventofcode2020.solutions.{{cookiecutter.file_name}} import (
+    {{cookiecutter.class_name}}PartB,
+)
 
 
 class Test{{cookiecutter.class_name}}PartB:
-    @pytest.mark.skip("This code is not yet implemented.")
     @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
     def test_{{cookiecutter.file_name}}b_solve(self, input_data, expected_result):
         solution = {{cookiecutter.class_name}}PartB()
         result = solution.solve(input_data)
         assert result == expected_result
 
-    @pytest.mark.skip("This code is not yet implemented.")
     def test_{{cookiecutter.file_name}}b_data(self):
         """ Result we got when we did the real solution """
         solution = {{cookiecutter.class_name}}PartB()
