e295fe930dbeb223b0c68794d45bf1f24d44368c
Roel Adriaans <roel.adriaans@ordina.nl>
2021-11-14 20:01:01 +0100

    Fix empty lines for day 1

--- src/adventofcode2020/solutions/day01.py
+++ src/adventofcode2020/solutions/day01.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class Day01:
     @staticmethod
     def compute_factor(input_data, n):
-        ints = [int(x) for x in input_data.split("\n")]
+        ints = [int(x) for x in input_data.split("\n") if x]
         for x in combinations(ints, n):
             if sum(x) == 2020:
                 logger.debug(f"Found the values {x}")
