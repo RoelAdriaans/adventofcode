724fe01500e429fb86480eca50ba95a2a72af5ba
Roel Adriaans <roel@adriaans.org>
2021-11-17 20:08:00 +0100

    Solution day 16, part 1

--- tests/solutions/test_day16_a.py
+++ tests/solutions/test_day16_a.py
@@ -1,5 +1,3 @@
-import pytest
-
 from adventofcode2020.solutions.day16 import Day16PartA, Ticket
 
 
@@ -14,14 +12,26 @@ class TestDay16PartA:
         assert not ticket.is_valid(4)
         assert ticket.is_valid(5)
 
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day16a_solve(self, input_data, expected_result):
+    def test_day16a_solve(self):
+        test_data = """class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+"""
         solution = Day16PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
+        result = solution.solve(test_data)
+        assert result == 71
 
     def test_day16a_data(self):
         """Result we got when we did the real solution"""
         solution = Day16PartA()
         res = solution("day_16/day16.txt")
-        assert res == 0
+        assert res == 20048
