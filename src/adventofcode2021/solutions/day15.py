743d093a37abaeb8b1517395983e05e804e1b621
Roel Adriaans <roel.adriaans@asml.com>
2022-10-18 10:28:08 +0200

    Make way for part 2

--- src/adventofcode2021/solutions/day15.py
+++ src/adventofcode2021/solutions/day15.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from collections.abc import Iterator
 from typing import Generic
-
+from functools import cache
 from adventofcode2021.utils.abstract import FileReaderSolution
 from adventofcode2021.utils.node import Node, T
 from adventofcode2021.utils.priority_queue import PriorityQueue
 
 
-class Day15(Generic[T]):
+class Day15(Generic[T], ABC):
     cave: dict[tuple[int, int], int]
     max_x: int
     max_y: int
@@ -18,8 +19,6 @@ class Day15(Generic[T]):
         for x, row in enumerate(input_data):
             for y, value in enumerate(row):
                 self.cave[x, y] = int(value)
-        self.max_x = max(x for x, _ in self.cave)
-        self.max_y = max(y for _, y in self.cave)
 
     def repr_cave(self) -> str:
         lines = []
@@ -28,6 +27,21 @@ class Day15(Generic[T]):
                 "".join(str(self.cave[x, y]) for y in range(0, self.max_y + 1))
             )
         return "\n".join(lines)
+
+    @property
+    @abstractmethod
+    def max_x(self) -> int:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def max_y(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_value(self, x: int, y: int) -> int:
+        """Return the value at a location."""
+        raise NotImplementedError
 
     def find_astar(self):
         initial: T = (0, 0)
@@ -60,11 +74,13 @@ class Day15(Generic[T]):
 
             # check where we can go next and haven't explored
             for child in self._get_neighbours(*current_state):
-                new_cost: float = current_node.cost + self.cave[child]
+                child_value = self.get_value(*child)
+
+                new_cost: float = current_node.cost + child_value
 
                 if child not in explored or explored[child] > new_cost:
                     explored[child] = new_cost
-                    frontier.push(Node(child, current_node, new_cost, self.cave[child]))
+                    frontier.push(Node(child, current_node, new_cost, child_value))
         return None
 
     def _get_neighbours(self, x, y) -> Iterator[tuple[int, int]]:
@@ -84,6 +100,19 @@ class Day15(Generic[T]):
 
 
 class Day15PartA(Day15, FileReaderSolution):
+    def get_value(self, x: int, y: int) -> int:
+        return self.cave[x, y]
+
+    @property
+    @cache
+    def max_y(self) -> int:
+        return max(y for _, y in self.cave)
+
+    @property
+    @cache
+    def max_x(self) -> int:
+        return max(x for x, _ in self.cave)
+
     def solve(self, input_data: str) -> int:
         self.fill_cave(input_data.splitlines())
         path = self.find_astar()
@@ -95,5 +124,18 @@ class Day15PartA(Day15, FileReaderSolution):
 
 
 class Day15PartB(Day15, FileReaderSolution):
+    def get_value(self, x: int, y: int) -> int:
+        return self.cave[x, y]
+
+    @property
+    @cache
+    def max_x(self) -> int:
+        return max(x for x, _ in self.cave) * 5
+
+    @property
+    @cache
+    def max_y(self) -> int:
+        return max(y for _, y in self.cave) * 5
+
     def solve(self, input_data: str) -> int:
         raise NotImplementedError
