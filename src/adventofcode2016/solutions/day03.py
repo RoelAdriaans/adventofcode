3a8a713a45671ea891532c6a0511b4afc8986250
Roel Adriaans <roel@adriaans.org>
2023-06-28 09:30:09 +0200

    Day 3

--- src/adventofcode2016/solutions/day03.py
+++ src/adventofcode2016/solutions/day03.py
@@ -1,31 +1,47 @@
+import itertools
 import re
 
 from adventofcode2016.utils.abstract import FileReaderSolution
 
 
 class Day03:
-    pass
-
-
-class Day03PartA(Day03, FileReaderSolution):
     @staticmethod
     def is_valid_triangle(sides: list[int]) -> bool:
         """Return if this is a valid triangle"""
         sides = sorted(sides)
         return sides[0] + sides[1] > sides[2]
 
-    def solve(self, input_data: str) -> int:
+    @staticmethod
+    def _split_into_parts(input_data: str) -> list[list[int, int, int]]:
         find_digits_re = re.compile(r"\s*(\d+)\s+(\d+)\s+(\d+)\s*")
         lines = input_data.splitlines()
-        count_valid = 0
+        res = []
         for line in lines:
             m = re.match(find_digits_re, line)
             sides = [int(m.group(1)), int(m.group(2)), int(m.group(3))]
-            if self.is_valid_triangle(sides):
-                count_valid += 1
-        return count_valid
+            res.append(sides)
+        return res
+
+
+class Day03PartA(Day03, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        return sum(
+            self.is_valid_triangle(side) for side in self._split_into_parts(input_data)
+        )
 
 
 class Day03PartB(Day03, FileReaderSolution):
+    def find_triangles(self, input_lines: str) -> list[list[int, int, int]]:
+        """Parse the input and return a list of triangles"""
+        list_if_sides = self._split_into_parts(input_lines)
+        # Transpose
+        transposed = zip(*list_if_sides)
+        # And now, chain everything together to create one big list again, in groups
+        # of 3.
+        grouped = [iter(itertools.chain(*transposed))] * 3
+        return zip(*grouped)
+
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        return sum(
+            self.is_valid_triangle(side) for side in self.find_triangles(input_data)
+        )
--- tests/solutions/test_day03_b.py
+++ tests/solutions/test_day03_b.py
@@ -1,19 +1,16 @@
-import pytest
-
 from adventofcode2016.solutions.day03 import Day03PartB
 
 
 class TestDay03PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day03b_solve(self, input_data, expected_result):
+    def test_day03b_solve(self, testdata):
+        # We're not given how many triangles are ok in the test data,
+        # we can only match the triangles.
         solution = Day03PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
+        result = list(solution.find_triangles(testdata))
+        assert len(result) == 6
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day03b_data(self):
         """Result we got when we did the real solution"""
         solution = Day03PartB()
         res = solution("day_03/day03.txt")
-        assert res == 0
+        assert res == 1577
