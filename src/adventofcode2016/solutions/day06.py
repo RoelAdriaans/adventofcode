162464b0eca10c1442a1c5bb3ea8a39a21aa0650
Roel Adriaans <roel@adriaans.org>
2023-06-30 21:30:41 +0200

    Day 6, part 2, generic solution

--- src/adventofcode2016/solutions/day06.py
+++ src/adventofcode2016/solutions/day06.py
@@ -6,23 +6,24 @@ from adventofcode2016.utils.abstract import FileReaderSolution
 
 
 class Day06:
-    pass
-
-
-class Day06PartA(Day06, FileReaderSolution):
-    def solve(self, input_data: str) -> str:
+    def compute_checksum(self, input_data: str, position: int) -> str:
         lines = input_data.splitlines()
         # Transpose the list
         transposed = zip(*lines)
         res = []
         for line in transposed:
             cnt = Counter(line)
-            letter = cnt.most_common(1)[0][0]
+            letter = cnt.most_common()[position][0]
             res.append(letter)
 
         return "".join(res)
 
 
+class Day06PartA(Day06, FileReaderSolution):
+    def solve(self, input_data: str) -> str:
+        return self.compute_checksum(input_data, 0)
+
+
 class Day06PartB(Day06, FileReaderSolution):
-    def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+    def solve(self, input_data: str) -> str:
+        return self.compute_checksum(input_data, -1)
