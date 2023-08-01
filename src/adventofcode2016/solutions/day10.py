9d8faddc1fe5bc00018878e18d701e33ed6aeed8
Roel Adriaans <roel@adriaans.org>
2023-08-01 21:14:31 +0200

    [WIP] Create some bots

--- src/adventofcode2016/solutions/day10.py
+++ src/adventofcode2016/solutions/day10.py
@@ -4,35 +4,48 @@ from collections import defaultdict
 
 import attrs
 from adventofcodeutils.graph import Graph
+from adventofcodeutils.node import Node
 from adventofcodeutils.parsing import extract_digits_from_string
 
 from adventofcode2016.utils.abstract import FileReaderSolution
 
 
-class Output:
-    number: int
-
-
+@attrs.define
 class Bot:
     number: int
     # value n goes to bot x
     values: list[int] = attrs.field(init=False)
     low_output: int
+    type_low: str
+
     high_output: int
+    type_high: str
 
     @classmethod
     def from_string(cls, s: str) -> Bot:
         """Create a new bot from a string"""
-        bot, low, high = extract_digits_from_string(s)
-        return Bot(number=bot, low_output=low, high_output=high)
+        bot, n_low, n_high = extract_digits_from_string(s)
+        splits = s.split()
+        type_low, type_high = splits[5], splits[10]
+        valid_types = ("bot", "output")
+        if type_high not in valid_types or type_low not in valid_types:
+            raise ValueError("Invalid outputtype in %s", s)
+
+        return Bot(
+            number=bot,
+            low_output=n_low,
+            type_low=type_low,
+            high_output=n_high,
+            type_high=type_high,
+        )
 
 
 class Day10:
-    bots: list[Bot]
+    bots: dict[int, Bot]
 
     def parse(self, input_data: str):
         """Parse the instructions"""
-        self.bots = []
+        self.bots = {}
         # Temp dict to move the 'value x goes to bot n' strings for now
         values = defaultdict(list)
         # Create the bots
