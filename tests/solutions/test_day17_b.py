34bfd44fc7595c8805ad8eb340736ac50d3f81b5
Roel Adriaans <roel@adriaans.org>
2023-08-20 21:18:01 +0200

    Day 17, add tests

--- tests/solutions/test_day17_b.py
+++ tests/solutions/test_day17_b.py
@@ -4,14 +4,19 @@ from adventofcode2016.solutions.day17 import Day17PartB
 
 
 class TestDay17PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result"),
+        [
+            ("ihgpwlah", 370),
+            ("kglvqrro", 492),
+            ("ulqzkmiv", 830),
+        ],
+    )
     def test_day17b_solve(self, input_data, expected_result):
         solution = Day17PartB()
         result = solution.solve(input_data)
         assert result == expected_result
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day17b_data(self):
         """Result we got when we did the real solution"""
         solution = Day17PartB()
