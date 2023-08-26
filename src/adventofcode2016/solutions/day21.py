c3c24b826b955fc1114c551869cb71eb08fc21f8
Roel Adriaans <roel@adriaans.org>
2023-08-26 21:30:47 +0200

    Day 21, part 2. Where the deque created a lot of issues, and a lot of copies needed to be created..

--- src/adventofcode2016/solutions/day21.py
+++ src/adventofcode2016/solutions/day21.py
@@ -9,32 +9,76 @@ from adventofcode2016.utils.abstract import FileReaderSolution
 
 class Day21:
     @staticmethod
-    def run_instructions(instructions: list[str], starting: str) -> str:
+    def rotate_base_on_letter(password, letter):
+        index = password.index(letter) + 1
+        if index > 4:
+            index += 1
+        password.rotate(index)
+        return password
+
+    @staticmethod
+    def run_instructions(
+        instructions: list[str], starting: str, descramble: bool = False
+    ) -> str:
         password = deque(starting.strip())
         for instruction in instructions:
             match regex_spm.fullmatch_in(instruction):
                 case r"swap position (?P<x>\d) with position (?P<y>\d)" as m:
-                    x, y = int(m["x"]), int(m["y"])
+                    if descramble:
+                        x, y = int(m["y"]), int(m["x"])
+                    else:
+                        x, y = int(m["x"]), int(m["y"])
                     password[x], password[y] = password[y], password[x]
 
                 case r"swap letter (?P<x>\D) with letter (?P<y>\D)" as m:
-                    x = password.index(m["x"])
-                    y = password.index(m["y"])
+                    if descramble:
+                        x = password.index(m["y"])
+                        y = password.index(m["x"])
+                    else:
+                        x = password.index(m["x"])
+                        y = password.index(m["y"])
+
                     password[x], password[y] = password[y], password[x]
 
                 case r"rotate left (?P<x>\d) steps?" as m:
-                    password.rotate(int(m["x"]) * -1)
+                    if descramble:
+                        password.rotate(int(m["x"]))
+                    else:
+                        password.rotate(int(m["x"]) * -1)
+
                 case r"rotate right (?P<x>\d) steps?" as m:
-                    password.rotate(int(m["x"]))
+                    if descramble:
+                        password.rotate(int(m["x"]) * -1)
+                    else:
+                        password.rotate(int(m["x"]))
+
                 case r"rotate based on position of letter (?P<x>\D)" as m:
-                    index = password.index(m["x"]) + 1
-                    if index > 4:
-                        index += 1
-                    password.rotate(index)
+                    if descramble:
+                        # To descramble, we brute force, and check if the result of
+                        # "abcde" is the same as "cdefb".rotate(x) == "abcde".
+                        # We rotate to the left, since the original will rotate to the
+                        # right.
+                        for n in range(len(password) + 2):
+                            hash = password.copy()
+                            hash.rotate(n * -1)
+                            to_validate = hash.copy()
+                            to_validate = Day21.rotate_base_on_letter(
+                                to_validate, m["x"]
+                            )
+                            if to_validate == password:
+                                password = hash
+                                break
+                        else:
+                            raise ValueError(
+                                "No solution found for %s", "".join(password)
+                            )
+                    else:
+                        password = Day21.rotate_base_on_letter(password, m["x"])
 
                 case r"reverse positions (?P<x>\d) through (?P<y>\d)" as m:
                     # Stupid slow, but it might just work:
                     x, y = int(m["x"]), int(m["y"]) + 1
+
                     password_list = list(password)
                     start, rot, end = (
                         password_list[:x],
@@ -44,7 +88,11 @@ class Day21:
                     password = deque([*start, *rot, *end])
 
                 case r"move position (?P<x>\d) to position (?P<y>\d)" as m:
-                    x, y = int(m["x"]), int(m["y"])
+                    if descramble:
+                        x, y = int(m["y"]), int(m["x"])
+                    else:
+                        x, y = int(m["x"]), int(m["y"])
+
                     # First rotate the position into place
                     password.rotate(x * -1)
                     remove = password.popleft()
@@ -67,5 +115,6 @@ class Day21PartA(Day21, FileReaderSolution):
 
 
 class Day21PartB(Day21, FileReaderSolution):
-    def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+    def solve(self, input_data: str) -> str:
+        instructions = list(reversed(input_data.splitlines()))
+        return self.run_instructions(instructions, "fbgdceah", descramble=True)
--- tests/solutions/test_day21_b.py
+++ tests/solutions/test_day21_b.py
@@ -4,16 +4,39 @@ from adventofcode2016.solutions.day21 import Day21PartB
 
 
 class TestDay21PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day21b_solve(self, input_data, expected_result):
-        solution = Day21PartB()
-        result = solution.solve(input_data)
+    @pytest.mark.parametrize(
+        ("instruction", "input", "expected_result"),
+        [
+            (
+                "rotate based on position of letter e",
+                "eabcd",
+                "abcde",
+            ),  # Should rotate 6 letters (len + 1)
+            ("rotate based on position of letter d", "decab", "ecabd"),
+            ("rotate based on position of letter b", "ecabd", "abdec"),
+            ("move position 3 to position 0", "abdec", "bdeac"),
+            ("move position 1 to position 4", "bdeac", "bcdea"),
+            ("rotate left 1 step", "bcdea", "abcde"),
+            ("reverse positions 0 through 4", "abcde", "edcba"),
+            ("swap letter d with letter b", "edcba", "ebcda"),
+            ("swap position 4 with position 0", "ebcda", "abcde"),
+        ],
+    )
+    def test_run_instructions(self, instruction, input, expected_result):
+        result = Day21PartB().run_instructions(
+            [instruction], starting=input, descramble=True
+        )
         assert result == expected_result
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
+    def test_day21b_solve(self, testdata):
+        solution = Day21PartB()
+        result = solution.run_instructions(
+            list(reversed(testdata.splitlines())), "decab", descramble=True
+        )
+        assert result == "abcde"
+
     def test_day21b_data(self):
         """Result we got when we did the real solution"""
         solution = Day21PartB()
         res = solution("day_21/day21.txt")
-        assert res == 0
+        assert res == "hegbdcfa"
