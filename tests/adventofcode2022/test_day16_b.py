633d9b7e8b83f4edcc8969791808c045de086b29
Roel Adriaans <roel.adriaans@asml.com>
2023-11-29 14:54:47 +0100

    Day 16, part 2

--- tests/adventofcode2022/test_day16_b.py
+++ tests/adventofcode2022/test_day16_b.py
@@ -1,25 +1,14 @@
-import pytest
-
 from adventofcode2022.day16 import Day16PartB
 
 
 class TestDay16PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day16b_testdata(self, testdata):
         solution = Day16PartB()
         result = solution.solve(testdata)
-        assert result == 0
+        assert result == 1707
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day16b_solve(self, input_data, expected_result):
-        solution = Day16PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day16b_data(self):
         """Result we got when we did the real solution"""
         solution = Day16PartB()
         res = solution("day_16/day16.txt")
-        assert res == 0
+        assert res == 2528
