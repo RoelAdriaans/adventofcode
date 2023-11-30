597d6057f75f22c89d140caca30518d82a5029f2
Roel Adriaans <roel@adriaans.org>
2023-11-30 22:39:34 +0100

    [WIP] WOrking on day 17

--- src/adventofcode2022/day17.py
+++ src/adventofcode2022/day17.py
@@ -1,27 +1,157 @@
 from __future__ import annotations
 
-import itertools
+import logging
+from enum import Enum
 from itertools import cycle
 
 from adventofcode.utils.abstract import FileReaderSolution
 
+logger = logging.getLogger(__name__)
+
+
+class Tile(Enum):
+    EMPTY = 0
+    FALLING = 1
+    ROCK = 2
+
+    @staticmethod
+    def show(tile) -> str:
+        values = {
+            Tile.EMPTY: ".",
+            Tile.ROCK: "#",
+            Tile.FALLING: "@",
+        }
+        return values[tile]
+
 
 class Day17:
     pieces = {
-        "-": [[1, 1, 1, 1]],
-        "+": [[0, 1, 0], [1, 1, 1], [0, 1, 0]],
-        "┘": [[0, 0, 1], [0, 0, 1], [1, 1, 1]],
-        "|": [[1], [1], [1], [1]],
-        "▉": [[1, 1], [1, 1]],
+        "-": [
+            [0, 0, 1, 1, 1, 1, 0],
+        ],
+        "+": [
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ],
+        "┘": [
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+        ],
+        "|": [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+        ],
+        "▉": [
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+        ],
     }
 
-    def piece_generator(self) -> cycle[tuple[str, list[list[int]]]]:
-        return itertools.cycle(self.pieces.items())
+    block_generator: cycle[tuple[str, list[list[int]]]]
+    step_generator: cycle[str]
+    room: list[list[Tile]]
+    width: int
+
+    def _block_generator(self) -> cycle[tuple[str, list[list[int]]]]:
+        return cycle(self.pieces.items())
+
+    @staticmethod
+    def _step_generator(instructions: str) -> cycle[str]:
+        return cycle(instructions.strip())
+
+    def create_room(self, instructions: str, width: int = 7):
+        """Create a room, `width` tiles wide, that follows `instructions`."""
+        # First ini
+        self.block_generator = self._block_generator()
+        self.step_generator = self._step_generator(instructions)
+        self.room = [[Tile.EMPTY] * width for _ in range(3)]
+        self.width = width
+
+    def repr_room(self) -> str:
+        """Represent a room"""
+        output = []
+        for row in self.room:
+            line = ["|"]
+            for item in row:
+                line.append(Tile.show(item))
+            line.append("|")
+            output.append("".join(line))
+        return "\n".join(output)
+
+    def drop_block(self):
+        """Drop the next block, and keep looping until it hits the bottom"""
+        piece, blocks = next(self.block_generator)
+        logger.debug("Next piece: %s", piece)
+        # Add a new block in the top
+        self.room.reverse()
+        for line in blocks:
+            self.room.append([Tile(i) for i in line])
+
+        self.room.reverse()
+        print(self.repr_room())
+        print()
+
+        while True:
+            step = next(self.step_generator)
+            logger.debug("Step: %s", step)
+            if step == "<":
+                idx = -1
+            else:
+                idx = 1
+
+            # First, check if any of the pieces is against a wall
+            hit_wall = False
+            for line in self.room:
+                if (
+                    not hit_wall
+                    and line[0] == Tile.FALLING
+                    or line[self.width] == Tile.FALLING
+                ):
+                    hit_wall = True
+                    logger.debug("Piece hit a wall, setting hit_wall to true")
+                    continue
+
+            # Next, can we move left or right?
+            if not hit_wall:
+                for line in self.room:
+                    if Tile.FALLING in line:
+                        line = self.move_line(line)
+
+            print(self.repr_room())
+            print()
+            return
+
+    def add_headroom(self):
+        """After a block is dropped, validate that we have enough room in the top for the
+        next block"""
+        ...
+
+    def calculate_height(self):
+        """Calculate the heigth of the tower"""
+        return -1
+
+    @staticmethod
+    def move_line(idx, line):
+        """Move a line left or right"""
+        return [x for x in line]
 
 
 class Day17PartA(Day17, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        self.create_room(instructions=input_data, width=7)
+        print(self.repr_room())
+        print()
+
+        for i in range(1):
+            logger.debug("Loop %s", i)
+            self.drop_block()
+            self.add_headroom()
+
+        return self.calculate_height()
 
 
 class Day17PartB(Day17, FileReaderSolution):
--- tests/adventofcode2022/test_day17_a.py
+++ tests/adventofcode2022/test_day17_a.py
@@ -1,21 +1,34 @@
 import pytest
 
-from adventofcode2022.day17 import Day17PartA
+from adventofcode2022.day17 import Day17PartA, Tile
 
 
 class TestDay17PartA:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
+    @pytest.mark.parametrize(
+        ("input_line", "idx", "expected_result"),
+        [
+            ("..@@@@.", 1, "...@@@@"),
+            ("...@@@@", 1, "...@@@@"),
+            ("..@@@@#", 1, "..@@@@#"),  # Cannot move to the right anymore
+            ("#...@.#", 1, "#....@#"),  # Move one to the right, keep empty
+            ("#....@#", 1, "#...@.#"),  # Move one to the left, keep empty
+        ],
+    )
+    def test_move_line(self, input_line, idx, expected_result):
+        # First build the line
+        mapping = str.maketrans(".@#", "012")
+        input_tiles = [Tile(int(char)) for char in input_line.translate(mapping)]
+        expected_output_tiles = [
+            Tile(int(char)) for char in expected_result.translate(mapping)
+        ]
+
+        output = Day17PartA.move_line(idx, input_tiles)
+        assert output == expected_output_tiles
+
     def test_day17a_testdata(self, testdata):
         solution = Day17PartA()
         result = solution.solve(testdata)
-        assert result == 0
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day17a_solve(self, input_data, expected_result):
-        solution = Day17PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
+        assert result == 3068
 
     @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day17a_data(self):
