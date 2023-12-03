da921a532709110f4f1d73b52dded05d8af1a9da
Roel Adriaans <roel@adriaans.org>
2023-12-03 21:55:13 +0100

    2023, day 3,

--- src/adventofcode2023/day03.py
+++ src/adventofcode2023/day03.py
@@ -1,12 +1,24 @@
 from __future__ import annotations
 
+from collections import defaultdict
+from math import prod
+
+import attrs
+
 from adventofcode.utils.abstract import FileReaderSolution
+
+
+@attrs.define(frozen=True)
+class Symbol:
+    symbol: str
+    row: int
+    col: int
 
 
 class Day03:
     grid: list[list[str]]
 
-    def parse_into_parts(self, input_data: str) -> list[int]:
+    def parse_into_parts(self, input_data: str) -> list[tuple[int, Symbol]]:
         """Parse into a list of integers with symbols connected"""
         # Create a row/col grid from the input data
         self.grid = [list(line) for line in input_data.splitlines()]
@@ -21,27 +33,28 @@ class Day03:
                     current_digits.append(self.grid[row][col])
                 else:
                     # Not a digit anymore, validate
-                    if current_digits and self.validate(current_digits, row, col - 1):
-                        part_numbers.append(int("".join(current_digits)))
+                    if current_digits and (
+                        symbol := self.validate(current_digits, row, col - 1)
+                    ):
+                        part_numbers.append((int("".join(current_digits)), symbol))
                     is_digit = False
                     current_digits = []
             # We have reached the edge, check if we have a valid digit in the bugger
-            if is_digit:
-                if self.validate(current_digits, row, col):
-                    part_numbers.append(int("".join(current_digits)))
+            if is_digit and (symbol := self.validate(current_digits, row, col)):
+                part_numbers.append((int("".join(current_digits)), symbol))
 
         return part_numbers
 
     @staticmethod
-    def check_symbol(input_value: str):
+    def check_symbol(input_value: str) -> bool:
         if input_value.isdigit():
             return False
         if input_value == ".":
             return False
         return True
 
-    def validate(self, digits, row, col) -> bool:
-        """Validate if the digits in are real"""
+    def validate(self, digits, row, col) -> bool | Symbol:
+        """Validate if the digits in are real. If they are real, return a Symbol"""
         # If we end at column 5 with 3 digits, we need to check
         # (eg, col - len(digits) ...  col+1)
         # 23456
@@ -52,7 +65,9 @@ class Day03:
             for check_col in range(min_col, max_col + 1):
                 try:
                     if self.check_symbol(self.grid[check_row][check_col]):
-                        return True
+                        return Symbol(
+                            self.grid[check_row][check_col], check_row, check_col
+                        )
                 except IndexError:
                     # Index out of bounds, ignoring
                     pass
@@ -62,10 +77,25 @@ class Day03:
 class Day03PartA(Day03, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         part_numbers = self.parse_into_parts(input_data)
-        res = sum(part_numbers)
+        res = sum([number for number, _ in part_numbers])
         return res
 
 
 class Day03PartB(Day03, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        part_numbers = self.parse_into_parts(input_data)
+        # Find the symbols with a *
+        valid_parts = [
+            (value, symbol) for value, symbol in part_numbers if symbol.symbol == "*"
+        ]
+        # Convert the row/col locations to see if we have duplicates
+        locations: defaultdict[Symbol, list] = defaultdict(list)
+        for amount, symbol in valid_parts:
+            locations[symbol].append(amount)
+
+        # Add the total if we have 2 (or more?!?) amounts on one location
+        total = 0
+        for value in locations.values():
+            if len(value) >= 2:
+                total += prod(value)
+        return total
