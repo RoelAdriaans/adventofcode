856dd48668c2e29385acb1d03fc49492edf0abc6
Roel Adriaans <roel@adriaans.org>
2023-12-09 21:54:19 +0100

    Day 9, part two is the same but reversed.. :)

--- src/adventofcode2023/day09.py
+++ src/adventofcode2023/day09.py
@@ -5,7 +5,7 @@ from adventofcode.utils.abstract import FileReaderSolution
 
 class Day09:
     @staticmethod
-    def calculate_line(line: str) -> int:
+    def create_differences(line: str) -> list[list[int]]:
         numbers = [int(digit) for digit in line.split()]
         differences = [numbers]
         while set(numbers) != {0}:
@@ -13,6 +13,10 @@ class Day09:
                 numbers[idx + 1] - number for idx, number in enumerate(numbers[:-1])
             ]
             differences.append(numbers)
+        return differences
+
+    def calculate_line(self, line: str) -> int:
+        differences = self.create_differences(line)
         # Adding numbers:
         # I need to add current last one + previous last one and add that to previous
         differences.reverse()
@@ -20,6 +24,21 @@ class Day09:
         for idx, line in enumerate(differences[:-1]):
             line.append(previous_add)
             previous_add = differences[idx + 1][-1] + previous_add
+
+        return previous_add
+
+    def calculate_backwards_line(self, line: str) -> int:
+        """Do the same as calculate_line but backwards..."""
+        differences = self.create_differences(line)
+        differences.reverse()
+        reversed_differences = []
+        for line in differences:
+            reversed_differences.append(list(reversed(line)))
+
+        previous_add = 0
+        for idx, line in enumerate(reversed_differences[:-1]):
+            line.append(previous_add)
+            previous_add = reversed_differences[idx + 1][-1] - previous_add
 
         return previous_add
 
@@ -31,4 +50,6 @@ class Day09PartA(Day09, FileReaderSolution):
 
 class Day09PartB(Day09, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        return sum(
+            [self.calculate_backwards_line(line) for line in input_data.splitlines()]
+        )
--- tests/adventofcode2023/test_day09_b.py
+++ tests/adventofcode2023/test_day09_b.py
@@ -1,25 +1,14 @@
-import pytest
-
 from adventofcode2023.day09 import Day09PartB
 
 
 class TestDay09PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    def test_day09b_testdata(self, testdata):
+    def test_day09b_testdata(self):
         solution = Day09PartB()
-        result = solution.solve(testdata)
-        assert result == 0
+        result = solution.solve("10  13  16  21  30  45")
+        assert result == 5
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day09b_solve(self, input_data, expected_result):
-        solution = Day09PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day09b_data(self):
         """Result we got when we did the real solution"""
         solution = Day09PartB()
         res = solution("day_09/day09.txt")
-        assert res == 0
+        assert res == 1131
