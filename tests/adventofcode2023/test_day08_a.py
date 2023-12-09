57ca3dabe7a323ff2a2c7d4068695d1976cfb241
Roel Adriaans <roel@adriaans.org>
2023-12-09 19:15:55 +0100

    Day 8, part 1

--- tests/adventofcode2023/test_day08_a.py
+++ tests/adventofcode2023/test_day08_a.py
@@ -7,8 +7,8 @@ class TestDay08PartA:
     @pytest.mark.parametrize(
         ("postfix", "expected_result"),
         [
-            ("test", 2),
-            ("test2", 6),
+            ("test_1", 2),
+            ("test_2", 6),
         ],
     )
     def test_extended(self, testdata_by_postfix, expected_result):
@@ -16,16 +16,8 @@ class TestDay08PartA:
         result = solution.solve(testdata_by_postfix)
         assert result == expected_result
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day08a_solve(self, input_data, expected_result):
-        solution = Day08PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day08a_data(self):
         """Result we got when we did the real solution"""
         solution = Day08PartA()
         res = solution("day_08/day08.txt")
-        assert res == 0
+        assert res == 11567
