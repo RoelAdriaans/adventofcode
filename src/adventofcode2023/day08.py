7bd1dbdffd7385cbc952507efca69a144cb23e35
Roel Adriaans <roel@adriaans.org>
2023-12-09 15:43:22 +0100

    Add test data for day 8

---
+++ src/adventofcode2023/day08.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from adventofcode.utils.abstract import FileReaderSolution
+
+
+class Day08:
+    pass
+
+
+class Day08PartA(Day08, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        raise NotImplementedError
+
+
+class Day08PartB(Day08, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        raise NotImplementedError
