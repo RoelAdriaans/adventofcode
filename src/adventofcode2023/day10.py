e0bc0431db06ba1c86d9d35589c10f630ad58922
Roel Adriaans <roel.adriaans@asml.com>
2023-12-11 16:58:02 +0100

    Day 10, part 1

--- src/adventofcode2023/day10.py
+++ src/adventofcode2023/day10.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+
 from enum import Enum
 
 from adventofcode.utils.abstract import FileReaderSolution
-from adventofcodeutils.point import XYPoint as Point
+
 
 class Tile(Enum):
     NS = "|"  # is a vertical pipe connecting north and south.
@@ -30,6 +31,20 @@ class Tile(Enum):
     def has_west(self) -> bool:
         return "W" in self.name
 
+    @property
+    def connections(self) -> list[tuple[int, int]]:
+        """Return the connections from this Tile as direction tuples as (row, col)"""
+        options = []
+        if self.has_north:
+            options.append((-1, 0))
+        if self.has_east:
+            options.append((0, 1))
+        if self.has_south:
+            options.append((1, 0))
+        if self.has_west:
+            options.append((0, -1))
+        return options
+
 
 class Node:
     tile: Tile
@@ -54,6 +69,7 @@ class Node:
 
 class Day10:
     grid: dict[tuple[int, int], str]
+    start_point = tuple[int, int]
 
     def parse(self, input_data: str):
         self.grid = {}
@@ -61,21 +77,63 @@ class Day10:
             for col_idx, char in enumerate(row):
                 self.grid[(row_idx, col_idx)] = char
                 if char == "S":
-                    start_point = (col_idx, row_idx)
+                    self.start_point = (row_idx, col_idx)
 
+    def find_path(self) -> list[tuple[int, int]]:
         # We know our startpoint, which nodes connect?
+        # This is the opposite to the connections on a Tile
+        connections = []
+        for direction in [(1, 0, "N"), (0, 1, "W"), (-1, 0, "S"), (0, -1, "E")]:
+            nb_rc = (
+                self.start_point[0] + direction[0],
+                self.start_point[1] + direction[1],
+            )
+            try:
+                neighbour = self.grid[nb_rc]
+                if direction[2] in Tile(neighbour).name:
+                    connections.append(nb_rc)
+            except KeyError:
+                pass
+        if len(connections) != 2:
+            raise ValueError(
+                f"Starting point does not have 2 connections, but : {connections}"
+            )
 
+        # Let's follow the path
 
+        cur_point = connections[0]
+        path = [cur_point]
+        visited = {cur_point}
 
-        print(start_point)
+        while cur_point != self.start_point:
+            tile = Tile(self.grid[cur_point])
+            connections = [
+                (cur_point[0] + delta[0], cur_point[1] + delta[1])
+                for delta in tile.connections
+            ]
+            next_step = [
+                connection
+                for connection in connections
+                if connection not in visited and connection != self.start_point
+            ]
+            try:
+                next_step = next_step[0]
+            except IndexError:
+                return path
 
-        pass
+            path.append(next_step)
+            visited.add(next_step)
+            cur_point = next_step
+
+        return path
 
 
 class Day10PartA(Day10, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         self.parse(input_data)
-        return -1
+        path = self.find_path()
+        steps = (len(path) + 1) // 2
+        return steps
 
 
 class Day10PartB(Day10, FileReaderSolution):
