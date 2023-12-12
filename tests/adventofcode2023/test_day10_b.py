05cb776b19031bd86ed890a85f89d0311b174f3d
Roel Adriaans <roel@adriaans.org>
2023-12-12 22:16:21 +0100

    Day 10, part b. With some help

--- tests/adventofcode2023/test_day10_b.py
+++ tests/adventofcode2023/test_day10_b.py
@@ -7,9 +7,9 @@ class TestDay10PartB:
     @pytest.mark.parametrize(
         ("postfix", "expected_result"),
         [
-            # ("loop_4", 4),
+            ("loop_4", 4),
             ("loop_8", 8),
-            # ("loop_10", 10),
+            ("loop_10", 10),
         ],
     )
     def test_day10b_testdata(self, testdata_by_postfix, expected_result):
@@ -18,16 +18,9 @@ class TestDay10PartB:
         assert result == expected_result
 
     @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day10b_solve(self, input_data, expected_result):
-        solution = Day10PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day10b_data(self):
         """Result we got when we did the real solution"""
         solution = Day10PartB()
         res = solution("day_10/day10.txt")
         assert res > 132
-        assert res == 0
+        assert res == 449
