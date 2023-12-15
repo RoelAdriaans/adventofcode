f4ff812596fbd2051116fec31b893dc1cbe04c1e
Roel Adriaans <roel.adriaans@asml.com>
2023-12-15 09:08:10 +0100

    Day 12, part 1. It works, runtime of 12 seconds.. :/

--- src/adventofcode2023/day12.py
+++ src/adventofcode2023/day12.py
@@ -1,14 +1,48 @@
 from __future__ import annotations
 
+import logging
+
+import more_itertools
+
 from adventofcode.utils.abstract import FileReaderSolution
+
+logger = logging.getLogger(__name__)
 
 
 class Day12:
     def count_arrangements(self, line: str) -> int:
+        """Count the arrangements that this group can make.
+
+        - We need to change the `?` into `#` or `.`
+        - After the change, the groups still must be valid -> Check with is_valid
+        - Groups is in order (!!), eg 1, 2, 3 -> #.##.###, NOT ###.##.#
+        """
         # For a line, calculate how many arrangements we can make
         conditions, groups = line.split()
         groups = [int(g) for g in groups.split(",")]
-        return -1
+
+        # Indexes contain the indexes of all the ? characters
+        indexes = [idx for idx, char in enumerate(conditions) if char == "?"]
+
+        # Now, I want to replace them all with # or ?, and check if they are a valid
+        # arrangement.
+        valid_arrangements = set()
+        for replacements in more_itertools.powerset(indexes):
+            copy = list(conditions)
+            for replacement in replacements:
+                copy[replacement] = "#"
+            copy = "".join(copy).replace("?", ".")
+
+            if self.is_valid_arrangement(copy, groups):
+                logger.debug("Valid arrangement %s", copy)
+                valid_arrangements.add(copy)
+        return len(valid_arrangements)
+
+    @staticmethod
+    def is_valid_arrangement(line: str, groups: list[int]) -> bool:
+        """Test that this is a valid arrangement"""
+        part_lengths = [len(p) for p in line.split(".") if len(p) > 0]
+        return part_lengths == groups
 
 
 class Day12PartA(Day12, FileReaderSolution):
--- tests/adventofcode2023/test_day12_a.py
+++ tests/adventofcode2023/test_day12_a.py
@@ -10,14 +10,32 @@ class TestDay12PartA:
         assert result == 21
 
     @pytest.mark.parametrize(
+        ("arrangement", "groups", "expected_result"),
+        [
+            ("#.#.###", [1, 1, 3], True),
+            ("##..###", [1, 1, 3], False),
+            (".###..##.#..", [3, 2, 1], True),
+            ("#.#.###", [1, 1, 3], True),
+            (".#...#....###.", [1, 1, 3], True),
+            (".#.###.#.######", [1, 3, 1, 6], True),
+            ("####.#...#...", [4, 1, 1], True),
+            ("#....######..#####.", [1, 6, 5], True),
+            (".###.##....#", [3, 2, 1], True),
+        ],
+    )
+    def test_day12a_is_valid_arrangement(self, arrangement, groups, expected_result):
+        result = Day12PartA.is_valid_arrangement(arrangement, groups)
+        assert result == expected_result
+
+    @pytest.mark.parametrize(
         ("input_data", "expected_result"),
         [
             ("???.### 1,1,3", 1),
-            # (".??..??...?##. 1,1,3", 4),
-            # ("?#?#?#?#?#?#?#? 1,3,1,6", 1),
-            # ("????.#...#... 4,1,1", 1),
-            # ("????.######..#####. 1,6,5", 4),
-            # ("?###???????? 3,2,1", 10),
+            (".??..??...?##. 1,1,3", 4),
+            ("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+            ("????.#...#... 4,1,1", 1),
+            ("????.######..#####. 1,6,5", 4),
+            ("?###???????? 3,2,1", 10),
         ],
     )
     def test_day12a_solve(self, input_data, expected_result):
@@ -25,9 +43,8 @@ class TestDay12PartA:
         result = solution.count_arrangements(input_data)
         assert result == expected_result
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day12a_data(self):
         """Result we got when we did the real solution"""
         solution = Day12PartA()
         res = solution("day_12/day12.txt")
-        assert res == 0
+        assert res == 7017
