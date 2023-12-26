abe45602e0f2919a089c42818c3f16df81148fe3
Roel Adriaans <roel@adriaans.org>
2023-12-26 05:49:54 +0100

    Day 13...

--- tests/adventofcode2023/test_day13_a.py
+++ tests/adventofcode2023/test_day13_a.py
@@ -1,20 +1,37 @@
 import pytest
 
-from adventofcode2023.day13 import Day13PartA, PatternMatch
+from adventofcode2023.day13 import Day13PartA
 
 
 class TestDay13PartA:
     @pytest.mark.parametrize(
-        ("pattern_id", "horizontal", "line"),
+        ("pattern_id", "line"),
         [
-            # (0, False, 5),
-            (1, True, 4),
+            (0, 5),
+            (1, 4),
         ],
     )
-    def test_flip(self, testdata, pattern_id, horizontal, line):
+    def test_flip(self, testdata, pattern_id, line):
         pattern = testdata.split("\n\n")[pattern_id]
         match = Day13PartA().check_pattern(pattern)
-        assert match.horizontal == horizontal
+        assert match.line == line
+
+    @pytest.mark.parametrize(
+        ("pattern_id", "line"),
+        [
+            (0, 10),
+            # For the second, it needs to be the bottom reflection, since the
+            # reflection on line 6 isn't perfect...
+            (1, 12),
+            (2, 12),
+            (3, 1),
+            (4, 1),
+            (5, 1),
+        ],
+    )
+    def test_day13a_solutiondata(self, solutiondata, pattern_id, line):
+        pattern = solutiondata.split("\n\n")[pattern_id]
+        match = Day13PartA().check_pattern(pattern)
         assert match.line == line
 
     def test_day13a_testdata(self, testdata):
@@ -22,16 +39,8 @@ class TestDay13PartA:
         result = solution.solve(testdata)
         assert result == 405
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day13a_solve(self, input_data, expected_result):
-        solution = Day13PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day13a_data(self):
         """Result we got when we did the real solution"""
         solution = Day13PartA()
         res = solution("day_13/day13.txt")
-        assert res == 0
+        assert res == 36_015
