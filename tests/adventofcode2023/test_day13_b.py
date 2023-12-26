e3ce1b18b91e31d2ea65a1421e3f9f2c934bd838
Roel Adriaans <roel@adriaans.org>
2023-12-26 05:49:54 +0100

    Day 13...

--- tests/adventofcode2023/test_day13_b.py
+++ tests/adventofcode2023/test_day13_b.py
@@ -4,22 +4,20 @@ from adventofcode2023.day13 import Day13PartB
 
 
 class TestDay13PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    def test_day13b_testdata(self, testdata):
-        solution = Day13PartB()
-        result = solution.solve(testdata)
-        assert result == 0
+    @pytest.mark.parametrize(
+        ("pattern_id", "line"),
+        [
+            (0, 3),
+            (1, 1),
+        ],
+    )
+    def test_flip(self, testdata, pattern_id, line):
+        pattern = testdata.split("\n\n")[pattern_id]
+        match = Day13PartB().check_pattern(pattern, smudges=1)
+        assert match.line == line
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day13b_solve(self, input_data, expected_result):
-        solution = Day13PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day13b_data(self):
         """Result we got when we did the real solution"""
         solution = Day13PartB()
         res = solution("day_13/day13.txt")
-        assert res == 0
+        assert res == 35335
