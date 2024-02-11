dfa7fa31e67fbde5ef8b2d21b808cab7f57311da
Roel Adriaans <roel@adriaans.org>
2024-02-11 15:25:37 +0100

    Day 15, part 2, not bad. Just a lot of reading

--- tests/adventofcode2023/test_day15_b.py
+++ tests/adventofcode2023/test_day15_b.py
@@ -1,25 +1,46 @@
 import pytest
 
-from adventofcode2023.day15 import Day15PartB
+from adventofcode2023.day15 import Day15PartB, HashMap, Operation
 
 
 class TestDay15PartB:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result", "hash"),
+        [
+            (
+                "rn=1",
+                HashMap(label="rn", operation=Operation.EQUALS, focal_length=1),
+                0,
+            ),
+            (
+                "cm-",
+                HashMap(label="cm", operation=Operation.DASH, focal_length=None),
+                0,
+            ),
+            (
+                "pc=6",
+                HashMap(label="pc", operation=Operation.EQUALS, focal_length=6),
+                3,
+            ),
+        ],
+    )
+    def test_day15b_create_instruction(self, input_data, expected_result, hash):
+        hm = Day15PartB.create_instruction(input_data)
+        assert hm == expected_result
+        assert hm.hash == hash
+
     def test_day15b_testdata(self, testdata):
         solution = Day15PartB()
         result = solution.solve(testdata)
-        assert result == 0
+        assert result == 145
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
     def test_day15b_solve(self, input_data, expected_result):
         solution = Day15PartB()
         result = solution.solve(input_data)
         assert result == expected_result
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day15b_data(self):
         """Result we got when we did the real solution"""
         solution = Day15PartB()
         res = solution("day_15/day15.txt")
-        assert res == 0
+        assert res == 296921
