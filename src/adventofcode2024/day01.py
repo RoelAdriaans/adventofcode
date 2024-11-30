091afbdef70d0d09fef73d7465aeb06256d5a1d4
Roel Adriaans <roel@adriaans.org>
2024-11-30 10:36:19 +0100

    Prep work for AOC 2024

---
+++ src/adventofcode2024/day01.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from adventofcode.utils.abstract import FileReaderSolution
+
+
+class Day01:
+    pass
+
+
+class Day01PartA(Day01, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        raise NotImplementedError
+
+
+class Day01PartB(Day01, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        raise NotImplementedError
