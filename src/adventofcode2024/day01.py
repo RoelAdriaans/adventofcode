bae8c53332ca26621319f446cff2afa51933084a
Roel Adriaans <roel@adriaans.org>
2024-12-01 08:26:05 +0100

    Advent of code 2024, day 01

--- src/adventofcode2024/day01.py
+++ src/adventofcode2024/day01.py
@@ -1,17 +1,35 @@
 from __future__ import annotations
 
+from collections import Counter
+
 from adventofcode.utils.abstract import FileReaderSolution
 
 
 class Day01:
-    pass
+    def parse_numbers(self, input_data) -> tuple[list[int], list[int]]:
+        lines = input_data.splitlines()
+        left_numbers = []
+        right_numbers = []
+        for line in lines:
+            left, right = line.split("  ")
+            left_numbers.append(int(left))
+            right_numbers.append(int(right))
+        # Next sort the lists
+        left_numbers.sort()
+        right_numbers.sort()
+        return left_numbers, right_numbers
 
 
 class Day01PartA(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        left_numbers, right_numbers = self.parse_numbers(input_data)
+        return sum(
+            [abs(left - right) for left, right in zip(left_numbers, right_numbers)]
+        )
 
 
 class Day01PartB(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        left_numbers, right_numbers = self.parse_numbers(input_data)
+        cnt = Counter(right_numbers)
+        return sum([left * cnt[left] for left in left_numbers])
--- tests/adventofcode2024/test_day01_a.py
+++ tests/adventofcode2024/test_day01_a.py
@@ -1,25 +1,14 @@
-import pytest
-
 from adventofcode2024.day01 import Day01PartA
 
 
 class TestDay01PartA:
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day01a_testdata(self, testdata):
         solution = Day01PartA()
         result = solution.solve(testdata)
-        assert result == 0
+        assert result == 11
 
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day01a_solve(self, input_data, expected_result):
-        solution = Day01PartA()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.xfail(reason="Not yet implemented", raises=NotImplementedError)
     def test_day01a_data(self):
         """Result we got when we did the real solution"""
         solution = Day01PartA()
         res = solution("day_01/day01.txt")
-        assert res == 0
+        assert res == 1660292
