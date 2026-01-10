46e53177bb1d74408721a39ba5d98e68efe295b2
Roel Adriaans <roel@adriaans.org>
2026-01-10 22:32:07 +0100

    Day 7, recursive

--- src/adventofcode2025/day07.py
+++ src/adventofcode2025/day07.py
@@ -1,74 +1,78 @@
 from __future__ import annotations
 
-from collections import defaultdict
+from functools import cache
 
 from adventofcode.utils.abstract import FileReaderSolution
 
 
 class Day07:
-    @staticmethod
-    def compute_lab(input_data: str) -> tuple[int, int]:
-        # First, let's parse the input data into a set of splitters
-        splitters: set[tuple[int, int]] = set()
+    start: tuple[int, int]
+    splitters: set[tuple[int, int]]
+    height: int
+
+    def parse_grid(self, input_data: str):
         lines = input_data.splitlines()
-        start = None
+        self.height = len(lines)
+        self.splitters = set()
 
         for row, line in enumerate(lines):
             for col, char in enumerate(line):
                 if char == "S":
-                    start = (row, col)
+                    self.start = (row, col)
                 elif char == "^":
-                    splitters.add((row, col))
+                    self.splitters.add((row, col))
 
+
+class Day07PartA(Day07, FileReaderSolution):
+    def compute_lab(self) -> int:
+        """Compute part 1"""
         # Start the BEAM!
-        beams: set[tuple[int, int]] = {start}
-        paths: defaultdict[tuple[int, int], int] = defaultdict(int)
-        # paths[start] = 0
+        beams: set[tuple[int, int]] = {self.start}
         splits = 0
-        for _ in range(len(lines)):
+        for _ in range(self.height):
             new_beams = set()
             for beam in beams:
                 # Get the location below the beam. If it's a splitter,
                 # we add two new beams
                 below = (beam[0] + 1, beam[1])
                 # No splitter - just continue downwards
-                if below not in splitters:
+                if below not in self.splitters:
                     new_beams.add(below)
-                    # paths[below] += 1
 
-                elif below in splitters:
+                elif below in self.splitters:
                     beam_left = (below[0], below[1] - 1)
                     beam_right = (below[0], below[1] + 1)
                     new_beams.add(beam_left)
                     new_beams.add(beam_right)
-                    paths[beam_left] += 1
-                    paths[beam_right] += 1
                     splits += 1
 
             beams = new_beams
+        return splits
 
-        # for row in range(len(lines)+1):
-        #     for col in range(len(lines[0])):
-        #         if (row, col) == start:
-        #             print('S', end='')
-        #         elif (row, col) in split_locations:
-        #             print('X', end='')
-        #         elif (row, col) in splitters:
-        #             print('^', end='')
-        #         elif (row, col) in beam_locations:
-        #             print('|', end='')
-        #         else:
-        #             print('.', end='')
-        #     print()
-        count_paths = sum(paths.values())
-        return splits, count_paths
-
-
-class Day07PartA(Day07, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        return self.compute_lab(input_data)[0]
+        self.parse_grid(input_data)
+        return self.compute_lab()
 
 
 class Day07PartB(Day07, FileReaderSolution):
+    @cache
+    def compute_timelines(self, position: tuple[int, int] = None) -> int:
+        if position is None:
+            # First encounter, first position is start position
+            position = self.start
+        if position[0] >= self.height:
+            # Are we at the end?
+            return 1
+        # Are we on a splitter?
+        if position in self.splitters:
+            # Resolve left and right
+            return self.compute_timelines(
+                (position[0] + 1, position[1] - 1)
+            ) + self.compute_timelines((position[0] + 1, position[1] + 1))
+        else:
+            # Continue downwards
+            return self.compute_timelines((position[0] + 1, position[1]))
+
     def solve(self, input_data: str) -> int:
-        return self.compute_lab(input_data)[1]
+        self.parse_grid(input_data)
+        return self.compute_timelines()
